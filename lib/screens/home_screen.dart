87c767d608e79084d81f2c69cd1da2a6d80c4f62
myracledesign <weber.max.1988@live.de>
2020-02-04 13:09:25 +0100

    finished example flutter fingerprint

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,33 @@
+import 'package:fingerprint/screens/dashboard_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:local_auth/local_auth.dart';
 
 class HomeScreen extends StatelessWidget {
+  final LocalAuthentication localAuth = LocalAuthentication();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GestureDetector(
+        onTap: () async {
+          bool weCanCheckBiometrics = await localAuth.canCheckBiometrics;
+
+          if (weCanCheckBiometrics) {
+            bool authenticated = await localAuth.authenticateWithBiometrics(
+              localizedReason: "Authenticate to see your bank statement.",
+            );
+
+            if (authenticated) {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DashboardScreen(),
+                ),
+              );
+            }
+          }
+        },
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
